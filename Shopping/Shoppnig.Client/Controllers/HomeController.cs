87eaf5928bcb8a5b3aaf3cbc1b05d33615ac8b37
Mukesh Zala <mukesh_zala@dell.com>
2022-08-31 10:27:21 -0500

    Use HttpClient to retrieve product data from API

--- Shopping/Shoppnig.Client/Controllers/HomeController.cs
+++ Shopping/Shoppnig.Client/Controllers/HomeController.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Shoppnig.Client.Data;
 using Shoppnig.Client.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Shoppnig.Client.Controllers
@@ -13,15 +15,22 @@ namespace Shoppnig.Client.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient _httpClient; 
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController( IHttpClientFactory httpClientFactory,  ILogger<HomeController> logger)
         {
+            _httpClient = httpClientFactory.CreateClient("ShoppingAPIClient"); 
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View( ProductContext.Products );
+
+            var response = await _httpClient.GetAsync("/product");
+            var context = await response.Content.ReadAsStringAsync();
+            var productList = JsonConvert.DeserializeObject<IEnumerable<Product>>(context); 
+
+            return View(productList);
         }
 
         public IActionResult Privacy()
