8876a7dd85c20f846f7e794afea6f1db129443e2
Mukesh Zala <mukesh_zala@dell.com>
2022-08-31 09:32:28 -0500

    added product controller and launchsetting

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shoppnig.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger; 
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Product
+            {
+                Name = "asd"
+            }).ToArray(); 
+
+        }
+    }
+}
